5c83142b8a95f9eceb8816c7e36eeeffea372c12
Ian Carroll <icarroll@sesync.org>
2016-12-08 23:42:12 -0500

    updates before friday

--- tests.R
+++ tests.R
@@ -3,23 +3,24 @@ sysname <- Sys.info()['sysname']
 if (sysname == 'Darwin') {
   JAVA_HOME <- system('/usr/libexec/java_home', intern=TRUE)
   dyn.load(paste0(JAVA_HOME, '/jre/lib/server/libjvm.dylib'))
-  model.path <- file.path("..", "models", "Sample Models", "Biology", "Wolf Sheep Predation.nlogo")
+  model_path <- file.path("..", "models", "Sample Models", "Biology", "Wolf Sheep Predation.nlogo")
 } else {
-  model.path <- file.path("models", "Sample Models", "Biology", "Wolf Sheep Predation.nlogo")
+  model_path <- file.path("models", "Sample Models", "Biology", "Wolf Sheep Predation.nlogo")
 }
 
 # Tests
 library(RNetLogo)
 library(ggplot2)
 
+message("")
 message("Find the NetLogo.jar file in your NetLogo installation.")
 message("On a Mac, in might be within /Applications/NetLogo 5.3.1/.")
 message("On Windows, it might be in C:\\Program Files\\NetLogo 5.3.1\\app\\.")
 invisible(readline("Press return to continue ..."))
 
-nl.path <- dirname(file.choose())
-NLStart(nl.path, gui=FALSE)
-NLLoadModel(file.path(nl.path, model.path))
+nl_path <- dirname(file.choose())
+NLStart(nl_path, gui=FALSE)
+NLLoadModel(file.path(nl_path, model_path))
 NLCommand("setup")
 NLReport("count sheep")
 
