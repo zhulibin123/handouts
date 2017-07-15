65275df148e1f88d2c04ac1fab7cda019fce79ed
Ian Carroll <icarroll@sesync.org>
2017-07-15 14:16:19 +0000

    updating worksheets

--- worksheet-1.R
+++ worksheet-1.R
@@ -9,10 +9,9 @@ vals <- seq(...,
 
 counts ...
 
-## Lists
+## Exercise 2
 
-... <- list(...)
-... <- ...(list(1, 2), c(3, 4))
+...
 
 ## Factors
 
@@ -27,15 +26,39 @@ education <- ...(c("college", "highschool", "college", "middle"),
 
 ... data.frame(...)
 
-## Exercise 1
+## Exercise 3
 
 ...
 
-## Read CSV
+## Load data into R
 
-surveys <- ...(...)
+plots <- ...(...)
+animals <- ...(...)
 
-## Plots
+## Exercise 4
+
+...
+
+## Names
+
+...(df) <- c(...)
+
+## Subsetting ranges
+
+days <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+weekdays <- ...
+...
+
+## Exercise 5
+
+...
+
+
+## Exercise 6
+
+...
+
+## Base plotting
 
 plot(..., ...)
 
@@ -47,20 +70,6 @@ plot(..., ...)
 
 boxplot(...)
 
-## Names
-
-names(...) <- ...
-
-## Subsetting ranges
-
-days <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
-weekdays <- ...
-...
-
-## Exercise 2
-
-...
-
 ## Anatomy of a function
 
 function(...) {
@@ -68,27 +77,37 @@ function(...) {
   return(...)
 }
 
-## Exercise 3
+## Flow control
+
+if (...) {
+    ...
+} else {
+    ...
+}
+
+firts <- function(...) {
+    if (...) {
+        ...
+    } else {
+        ...
+    }
+}
+
+## Exercise 7
 
 ...
 
 ## Distributions and statistics
+
+samp <- ...
+
 
 x <- rnorm(..., mean = .., sd = ...)
 y <- r...(n = 100, size = 50, ...)
 
 fit <- ...
 
-## Exercise 4
+## Exercise 8
 
 ...
 
-## Install missing packages
-
-requirements <- c(...)
-installed <- rownames(installed.packages())
-missing <- setdiff(..., ...)
-                   
-if (...) {
-  install.packages(missing)
-}
