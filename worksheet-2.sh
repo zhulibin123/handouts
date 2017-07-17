ed0f669bd134bad545be72de401aa6561661e9e5
Ian Carroll <itcarroll@users.noreply.github.com>
2017-07-17 08:55:05 -0400

    Update worksheet-2.sh

--- worksheet-2.sh
+++ worksheet-2.sh
@@ -8,3 +8,7 @@ git config --global user.email ...
 ## Change the "origin" remote URL and push
 git remote set-url origin ...
 git push --all
+
+## Set upstream
+#git remote add upstream https://github.com/sesync-ci/handouts
+#git pull upstream master
