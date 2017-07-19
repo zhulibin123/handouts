5017356f8def2cf5cb0f7cba0ca727988c4c6b48
Ian Carroll <itcarroll@users.noreply.github.com>
2017-07-19 10:49:44 -0400

    Update worksheet-6.R

--- worksheet-6.R
+++ worksheet-6.R
@@ -1,20 +1,50 @@
+## lm
+
+animals <- read.csv('data/animals.csv', stringsAsFactors = FALSE, na.strings = '')
+fit <- lm(
+  ...,
+  data = animals)
+
+
+animals$species_id <- ...
+fit <- lm(
+  ...,
+  data = animals)
+
+## glm
+
+animals$sex <- ...
+fit <- glm(sex ~ hindfoot_length,
+           ...,
+           data = animals)
+
+## lme4
+
+# install.packages('lme4')
+
+library(lme4)
+fit <- lmer(...,
+            data = animals)
+
+fit <- lmer(...,
+            data = animals)
+
+## RStan
+
 library(dplyr)
 library(rstan)
-
-animals <- read.csv('data/animals.csv', na.strings = "", stringsAsFactors = FALSE)
-species <- read.csv('data/species.csv', na.strings = "", stringsAsFactors = FALSE)
-
 stanimals <- animals %>%
-  filter(year == 1980) %>%
-  inner_join(species, c('species_id' = 'id')) %>%
-  select(weight, genus, hindfoot_length) %>%
+  select(weight, species_id, hindfoot_length) %>%
   na.omit() %>%
-  mutate(
-    log_weight = log(weight),
-    genus = as.integer(factor(genus))) %>%
-  select(-weight) %>%
-  as.list() %>%
-  c(N = length(.[[1]]), M = max(.[['genus']]))
+  mutate(log_weight = log(weight),
+         species_idx = as.integer(factor(species_id))) %>%
+  select(-weight, -species_id)
+stanimals <- c(
+  N = nrow(stanimals),
+  M = max(stanimals$species_idx),
+  as.list(stanimals))
 
-fit <- stan(file = 'worksheet.stan', data = stanimals,
-            iter = 100, chains = 1)
+samp <- stan(file = 'worksheet-6.stan',
+             data = stanimals,
+             iter = 1000, chains = 3)
+saveRDS(samp, 'stanimals.RDS')
